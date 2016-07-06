e2add00ab9c61062e0f69b6c5247f13f177452dc
snooker155 <izub.anton@gmail.com>
2016-07-06 20:39:16 +0300

    base usage of genetic algorythm

--- client/main.js
+++ client/main.js
@@ -4,24 +4,72 @@ import { ReactiveVar } from 'meteor/reactive-var';
 import './main.html';
 
 
-Template.products.helpers({
-  products() {
-    return Products.find();
+Template.body.onCreated(function() {
+    var self = this;
+    // Tracker.autorun(function (c) {
+    //     self.generation = Generations.findOne({}, {sort: {generation_n: -1}});
+    //     //console.log("Now i am here");
+    //     self.c = c;
+        var generation_subscription = self.subscribe("generations");
+        if(generation_subscription.ready()){
+			console.log('Loaded!');
+        }else{
+            console.log('Loading...');
+        }
+    // });
+
+    self.getGeneration = function(){
+        return Generations.findOne({}, {sort: {generation_n: -1}});
+    }
+});
+
+
+Template.body.helpers({
+  all_products() {
+  	var generation = Generations.findOne({}, {sort: {generation_n: -1}});
+    if(generation){
+    	return generation.products_arr;
+    }
   },
 });
 
 
 Template.customers_stat.helpers({
-  products() {
-    return Products.find();
-  },
-
   customers_number(){
-	return Customers.find({}).count();
+  	var generation = Generations.findOne({}, {sort: {generation_n: -1}});
+  	if(generation){
+    	return generation.customers_arr.length;
+    }
   },
 
-  product_share(){
-  	return this.getShare();
-  }
+  generation_n(){
+	var generation = Generations.findOne({}, {sort: {generation_n: -1}});
+	if(generation){
+    	return generation.generation_n;
+    }
+  },
+
+  // product_share(){
+  // 	return this.getShare();
+  // },
+
+  // product_util(){
+  // 	return this.product_util;
+  // },
+
+  // share_sum(){
+  // 	var count = 0;
+  // 	Products.find().fetch().forEach(function (product) {
+  // 		count += product.getShare();
+  // 	});
+  // 	return parseFloat(count.toFixed(2));
+  // },
 });
 
+
+
+
+// Template.body.onDestroyed(function() {
+//     var self = this;
+//     self.c.stop();
+// });
--- server/main.js
+++ server/main.js
@@ -1,88 +1,122 @@
 import { Meteor } from 'meteor/meteor';
 
-var crossover = function(generation){
-	//console.log(generation.sort(function(a, b){return b.product_share - a.product_share}))
-	for (var i = 0; i < generation.length; i++){
-		var first_parent = Products.findOne({product_id: Math.floor(Math.random() * 5)});
-		var second_parent = Products.findOne({product_id: Math.floor(Math.random() * 5)});
-		while (first_parent.product_id == second_parent.product_id) {
-			second_parent = Products.findOne({product_id: Math.floor(Math.random() * 5)});
-		}
-		var child_prop = [];
+//var id = 0;
 
-		//console.log(first_parent.product_id+" --- "+second_parent.product_id);
+// var crossover = function(generation){
+// 	//console.log(generation.sort(function(a, b){return b.product_share - a.product_share}))
+// 	for (var i = 0; i < generation.length; i++){
+// 		var first_parent = Products.findOne({product_id: Math.floor(Math.random() * 5)});
+// 		var second_parent = Products.findOne({product_id: Math.floor(Math.random() * 5)});
+// 		while (first_parent.product_id == second_parent.product_id) {
+// 			second_parent = Products.findOne({product_id: Math.floor(Math.random() * 5)});
+// 		}
+// 		var child_prop = [];
 
-		for(var j = 0; j < 5; j++){
-			var selected_parent = null;
-			if(Math.random() >= 0.5){
-				selected_parent = first_parent;
-			}else{
-				selected_parent = second_parent;
-			}
-			//console.log(selected_parent.product_id+" --- "+selected_parent.prop[j].prop_name);
-			child_prop.push({
-				prop_name: selected_parent.prop[j].prop_name,
-			})
-		}
+// 		//console.log(first_parent.product_id+" --- "+second_parent.product_id);
 
-		var child = Products.insert({
-			product_id: Products.find({}).count()+1,
-			product_name: "Prod " + Products.find({}).count(),
-			product_price: 20,
-			prop: child_prop,
-			product_creator: "Bot",
-	        product_status: "Completed",
-	        product_share: 0,
-		});
-	}
-}
+// 		for(var j = 0; j < 6; j++){
+// 			var selected_parent = null;
+// 			if(Math.random() >= 0.5){
+// 				selected_parent = first_parent;
+// 			}else{
+// 				selected_parent = second_parent;
+// 			}
+// 			//console.log(selected_parent.product_id+" --- "+selected_parent.prop[j].prop_name);
+// 			var index = Math.floor(Math.random() * 6);
+// 			while(child_prop[index]){
+// 				index = Math.floor(Math.random() * 6);
+// 			}
+// 			child_prop.push({
+// 				prop_name: selected_parent.prop[index].prop_name,
+// 			})
+// 		}
+
+// 		var child = Products.insert({
+// 			product_id: Products.find({}).count()+1,
+// 			product_name: "Prod " + id,
+// 			product_price: 20,
+// 			prop: child_prop,
+// 			product_creator: "Bot",
+// 	        product_status: "Completed",
+// 	        //product_share: 0,
+// 	        product_util: 0,
+// 		});
+// 		id++;
+// 	}
+// }
 
 
-var mutation = function(generation){
+// var mutation = function(generation){
+// 	//console.log(generation);
+// 	generation.forEach(function (member) {
+// 		if(Math.random() < 0.5){
+// 			var new_index = Math.floor(Math.random() * 6);
+// 			var new_prop = "prop_"+Math.floor(Math.random() * 10);
+// 			member.prop[new_index].prop_name = new_prop;
+// 		}
 
-}
+// 		Products.update(member._id, {
+// 			product_id: member.product_id,
+// 			product_name: member.product_name,
+// 			product_price: member.product_price,
+// 			prop: member.prop,
+// 			product_creator: member.product_creator,
+// 		    product_status: member.product_status,
+// 		    //product_share: member.getShare(),
+// 		    product_util: member.getUtil(),
+// 		});
+// 	});
+// }
 
 
-var selection = function(generation){
-	var new_generation = generation.sort(function(a, b){return b.product_share - a.product_share}).slice(0, 6);
-	//console.log(generation.sort(function(a, b){return b.product_share - a.product_share}).slice(0, 6));
-	Products.find().fetch().forEach(function (product) {
-		Products.remove(product._id);
-	});
+// var selection = function(generation){
+// 	//var new_generation = generation.sort(function(a, b){return b.product_share - a.product_share}).slice(0, 6);
+// 	var new_generation = generation.sort(function(a, b){return b.product_util - a.product_util}).slice(0, 6);
+// 	//console.log(generation.sort(function(a, b){return b.product_share - a.product_share}).slice(0, 6));
+// 	Products.find().fetch().forEach(function (product) {
+// 		Products.remove(product._id);
+// 	});
 
-	var i = 0;
+// 	var i = 0;
 
-	new_generation.forEach(function (member) {
-		Products.insert({
-			product_id: i,
-			product_name: member.product_name,
-			product_price: member.product_price,
-			prop: member.prop,
-			product_creator: member.product_creator,
-	        product_status: member.product_status,
-	        product_share: member.product_share,
-		});
-		i++;
-	});
-}
+// 	new_generation.forEach(function (member) {
+// 		Products.insert({
+// 			product_id: i,
+// 			product_name: member.product_name,
+// 			product_price: member.product_price,
+// 			prop: member.prop,
+// 			product_creator: member.product_creator,
+// 	        product_status: member.product_status,
+// 	        //product_share: member.product_share,
+// 	        product_util: member.product_util,
+// 		});
+// 		i++;
+// 	});
+// }
 
 
 
 Meteor.startup(() => {
   
+  	//var id = 0;
 
-  	Customers.find().fetch().forEach(function (customer) {
-		Customers.remove(customer._id);
-	});
-
-
-	Products.find().fetch().forEach(function (product) {
-		Products.remove(product._id);
-	});
+ //  	Customers.find().fetch().forEach(function (customer) {
+	// 	Customers.remove(customer._id);
+	// });
 
 
-	Features.find().fetch().forEach(function (feature) {
-		Features.remove(feature._id);
+	// Products.find().fetch().forEach(function (product) {
+	// 	Products.remove(product._id);
+	// });
+
+
+	// Features.find().fetch().forEach(function (feature) {
+	// 	Features.remove(feature._id);
+	// });
+
+
+	Generations.find().fetch().forEach(function (generation) {
+		Generations.remove(generation._id);
 	});
 
 
@@ -116,22 +150,32 @@ Meteor.startup(() => {
 
 
 	var CUSTOMERS_NUMBER = 1000;
-	var FEATRUES_NUMBER = 10;
+	var FEATRUES_NUMBER = 20;
 	var PRODUCTS_NUMBER = 6;
+
+	var products = [];
+	var customers = [];
+	var features = [];
 
 
 
 	for (var i = 0; i < FEATRUES_NUMBER; i++){
-		Features.insert({
+
+		features.push({
 			feature_id: i,
 			feature_name: "prop_"+i,
-			// time_to_achieve: 10,
-			// feature_price: 450,
-			// neccessary_employees_number: 4,
-			// neccessary_department: "Technology",
-			// max_feature_level: 3,
-			//neccessary_level: 1,
 		});
+
+		// Features.insert({
+		// 	feature_id: i,
+		// 	feature_name: "prop_"+i,
+		// 	// time_to_achieve: 10,
+		// 	// feature_price: 450,
+		// 	// neccessary_employees_number: 4,
+		// 	// neccessary_department: "Technology",
+		// 	// max_feature_level: 3,
+		// 	//neccessary_level: 1,
+		// });
 	}
 
 
@@ -140,25 +184,156 @@ Meteor.startup(() => {
 
 		var prop = [];
 
-		for(var j = 0; j < 5; j++){
+		for(var j = 0; j < Math.floor(Math.random() * (features.length - 10.1) + 1); j++){
 			prop.push({
-				prop_name: "prop_"+Math.floor(Math.random() * 9),
+				prop_name: "prop_"+Math.floor(Math.random() * (features.length - 0.001)),
 			})
 		}
 
+		//var product_price = Math.pow(5, prop.length);
+		var product_price = 3
+		if(prop.length > 0){
+			product_price = 5 * prop.length;
+		}
 
-		Products.insert({
+
+		products.push({
 			product_id: i,
 			product_name: "Prod " + i,
 			//product_price: 10 + Math.floor(Math.random() * 20),
-			product_price: 20,
+			product_price: product_price,
 			//product_color: "lightblue",
 			prop: prop,
 			product_creator: "Bot",
 	        product_status: "Completed",
-	        product_share: 0,
+	        //product_share: 0,
+	        product_util: 0,
 		});
+
+
+		// Products.insert({
+		// 	product_id: i,
+		// 	product_name: "Prod " + id,
+		// 	//product_price: 10 + Math.floor(Math.random() * 20),
+		// 	product_price: 20,
+		// 	//product_color: "lightblue",
+		// 	prop: prop,
+		// 	product_creator: "Bot",
+	 //        product_status: "Completed",
+	 //        //product_share: 0,
+	 //        product_util: 0,
+		// });
+		// id++;
 	}
+
+
+
+	// products.push({
+	// 	product_id: 0,
+	// 	product_name: "Prod " + 0,
+	// 	product_price: 30,
+	// 	prop: [
+	// 		{prop_name: "prop_1"}, ///21
+	// 		{prop_name: "prop_2"},
+	// 		{prop_name: "prop_3"},
+	// 		{prop_name: "prop_4"},
+	// 		{prop_name: "prop_5"},
+	// 		{prop_name: "prop_6"},
+	// 	],
+	// 	product_creator: "Bot",
+	//     product_status: "Completed",
+	//     product_util: 0,
+	// });
+
+	// products.push({
+	// 	product_id: 1,
+	// 	product_name: "Prod " + 1,
+	// 	product_price: 30,
+	// 	prop: [
+	// 		{prop_name: "prop_1"}, ///26
+	// 		{prop_name: "prop_3"},
+	// 		{prop_name: "prop_4"},
+	// 		{prop_name: "prop_5"},
+	// 		{prop_name: "prop_6"},
+	// 		{prop_name: "prop_7"},
+	// 	],
+	// 	product_creator: "Bot",
+	//     product_status: "Completed",
+	//     product_util: 0,
+	// });
+
+
+	// products.push({
+	// 	product_id: 2,
+	// 	product_name: "Prod " + 2,
+	// 	product_price: 30,
+	// 	prop: [
+	// 		{prop_name: "prop_1"}, ///31
+	// 		{prop_name: "prop_4"},
+	// 		{prop_name: "prop_5"},
+	// 		{prop_name: "prop_6"},
+	// 		{prop_name: "prop_7"},
+	// 		{prop_name: "prop_8"},
+	// 	],
+	// 	product_creator: "Bot",
+	//     product_status: "Completed",
+	//     product_util: 0,
+	// });
+
+
+	// products.push({
+	// 	product_id: 3,
+	// 	product_name: "Prod " + 3,
+	// 	product_price: 30,
+	// 	prop: [
+	// 		{prop_name: "prop_1"}, ///18
+	// 		{prop_name: "prop_9"},
+	// 		{prop_name: "prop_2"},
+	// 		{prop_name: "prop_3"},
+	// 		{prop_name: "prop_1"},
+	// 		{prop_name: "prop_2"},
+	// 	],
+	// 	product_creator: "Bot",
+	//     product_status: "Completed",
+	//     product_util: 0,
+	// });
+
+
+	// products.push({
+	// 	product_id: 4,
+	// 	product_name: "Prod " + 4,
+	// 	product_price: 30,
+	// 	prop: [
+	// 		{prop_name: "prop_8"}, ///31
+	// 		{prop_name: "prop_2"},
+	// 		{prop_name: "prop_4"},
+	// 		{prop_name: "prop_2"},
+	// 		{prop_name: "prop_3"},
+	// 		{prop_name: "prop_2"},
+	// 	],
+	// 	product_creator: "Bot",
+	//     product_status: "Completed",
+	//     product_util: 0,
+	// });
+
+
+	// products.push({
+	// 	product_id: 5,
+	// 	product_name: "Prod " + 5,
+	// 	product_price: 30,
+	// 	prop: [
+	// 		{prop_name: "prop_8"}, ///24
+	// 		{prop_name: "prop_7"},
+	// 		{prop_name: "prop_3"},
+	// 		{prop_name: "prop_2"},
+	// 		{prop_name: "prop_2"},
+	// 		{prop_name: "prop_2"},
+	// 	],
+	// 	product_creator: "Bot",
+	//     product_status: "Completed",
+	//     product_util: 0,
+	// });
+
 
 
 
@@ -166,108 +341,262 @@ Meteor.startup(() => {
 
 		var needed = [];
 
-		for(var j = 0; j < 3; j++){
+		for(var j = 0; j < 10; j++){
 			needed.push({
 				value: 0,
 				weight: Math.floor(Math.random() * 10),  
+				//weight: 1,  
 				prop: {
-					prop_0: Math.floor(Math.random() * 4),
-					prop_1: Math.floor(Math.random() * 4),
-					prop_2: Math.floor(Math.random() * 4),
-					prop_3: Math.floor(Math.random() * 4),
-					prop_4: Math.floor(Math.random() * 4),
-					prop_5: Math.floor(Math.random() * 4),
-					prop_6: Math.floor(Math.random() * 4),
-					prop_7: Math.floor(Math.random() * 4),
-					prop_8: Math.floor(Math.random() * 4),
-					prop_9: Math.floor(Math.random() * 4),
+					// prop_0: Math.floor(Math.random() * 10),
+					// prop_1: Math.floor(Math.random() * 10),
+					// prop_2: Math.floor(Math.random() * 10),
+					// prop_3: Math.floor(Math.random() * 10),
+					// prop_4: Math.floor(Math.random() * 10),
+					// prop_5: Math.floor(Math.random() * 10),
+					// prop_6: Math.floor(Math.random() * 10),
+					// prop_7: Math.floor(Math.random() * 10),
+					// prop_8: Math.floor(Math.random() * 10),
+					// prop_9: Math.floor(Math.random() * 10),
+					// prop_10: Math.floor(Math.random() * 10),
+					// prop_11: Math.floor(Math.random() * 10),
+					// prop_12: Math.floor(Math.random() * 10),
+					// prop_13: Math.floor(Math.random() * 10),
+					// prop_14: Math.floor(Math.random() * 10),
+					// prop_15: Math.floor(Math.random() * 10),
+					// prop_16: Math.floor(Math.random() * 10),
+					// prop_17: Math.floor(Math.random() * 10),
+					// prop_18: Math.floor(Math.random() * 10),
+					// prop_19: Math.floor(Math.random() * 10),
+					prop_0: 0,
+					prop_1: 1,
+					prop_2: 2,
+					prop_3: 3,
+					prop_4: 4,
+					prop_5: 5,
+					prop_6: 6,
+					prop_7: 7,
+					prop_8: 8,
+					prop_9: 9,
+					prop_10: 10,
+					prop_11: 11,
+					prop_12: 12,
+					prop_13: 13,
+					prop_14: 14,
+					prop_15: 15,
+					prop_16: 16,
+					prop_17: 17,
+					prop_18: 18,
+					prop_19: 19,
 				}
 			})
 		}
 
-		Customers.insert({
+		// needed = [{
+		// 	value: 0,
+		// 	weight: 6,   
+		// 	prop: {
+		// 		prop_0: 0,
+		// 		prop_1: 1,
+		// 		prop_2: 2,
+		// 		prop_3: 3,
+		// 	}
+		// },{
+		// 	value: 0,
+		// 	weight: 3,   
+		// 	prop: {
+		// 		prop_0: 1,
+		// 		prop_1: 2,
+		// 		prop_2: 3,
+		// 		prop_3: 0,
+		// 	}
+		// },{
+		// 	value: 0,
+		// 	weight: 5,   
+		// 	prop: {
+		// 		prop_0: 2,
+		// 		prop_1: 3,
+		// 		prop_2: 0,
+		// 		prop_3: 1,
+		// 	}
+		// },{
+		// 	value: 0,
+		// 	weight: 4,   
+		// 	prop: {
+		// 		prop_0: 3,
+		// 		prop_1: 0,
+		// 		prop_2: 1,
+		// 		prop_3: 2,
+		// 	}
+		// }];
+
+
+		customers.push({
 			customer_id: i,
-			//customer_region: region.region_id,
-			//customer_pref: region.region_pref,
-			//customer_money: 2000 + Math.floor((Math.random() * 500) + 100),
-			//base_customer_conservatism: region.base_level_of_conservatism,
-			//customer_conservatism: null,
-			//customer_product_conservatism: {},
 			customer_income: 20 + Math.floor(Math.random() * 3),
-			//customer_pref: region.region_pref,
-			//customer_activity: 1,
 			customer_product: "",
-			//customer_product_quantity: 0,
-			//customer_neighbors: [],
-			//customer_adv: 0,
-			//customer_history: [],
 			needed: needed,
 		});
+
+
+		// Customers.insert({
+		// 	customer_id: i,
+		// 	//customer_region: region.region_id,
+		// 	//customer_pref: region.region_pref,
+		// 	//customer_money: 2000 + Math.floor((Math.random() * 500) + 100),
+		// 	//base_customer_conservatism: region.base_level_of_conservatism,
+		// 	//customer_conservatism: null,
+		// 	//customer_product_conservatism: {},
+		// 	customer_income: 20 + Math.floor(Math.random() * 3),
+		// 	//customer_pref: region.region_pref,
+		// 	//customer_activity: 1,
+		// 	customer_product: "",
+		// 	//customer_product_quantity: 0,
+		// 	//customer_neighbors: [],
+		// 	//customer_adv: 0,
+		// 	//customer_history: [],
+		// 	needed: needed,
+		// });
 	}
 
 
+	Generations.insert({
+		customers_arr: customers,
+		products_arr: products,
+		features_arr: features,
+		generation_n: 1,
+	});
 
+
+	var time_count = 1;
 
 	var interval = Meteor.setInterval(function(){
+	var target_util = 0;
+	//while(target_util != 162){
 
 	   	console.log("-----------------------------  START  --------------------------------");
 
-	   	var products = [];
-		var features = [];
-		var customers = [];
-
-		Products.find().fetch().forEach(function (product) {
-			products.push(product);
-		});
-
-		crossover(products);
 
 
 
-	   	Customers.find({}).fetch().forEach(function (customer) {
-	   		Customers.update(customer._id,{
-		   		customer_id: customer.customer_id,
-				customer_income: customer.customer_income,
-				customer_product: customer.selectProduct(),
-				needed: customer.needed,
-		   	});
-	   	});
-
-	   	Customers.find().fetch().forEach(function (customer) {
-			customers.push(customer);
-		});
-
-		Features.find().fetch().forEach(function (feature) {
-			features.push(feature);
-		});
+	   	//////////////////////////////////////////////////////////////////////////////////////////////////
+	   	//////////////////////////////////////////////////////////////////////////////////////////////////
+	   	//////////////////////////////////////////////////////////////////////////////////////////////////
 
 
-	   	Products.find({}).fetch().forEach(function (product) {
-	   		Products.update(product._id, {
-				product_id: product.product_id,
-				product_name: product.product_name,
-				product_price: product.product_price,
-				prop: product.prop,
-				product_creator: product.product_creator,
-		        product_status: product.product_status,
-		        product_share: product.getShare(),
-			});
+
+
+
+	   	time_count++;
+
+	   	var generation = Generations.findOne({}, {sort: {generation_n: -1}});
+	   	// var generation = Generations.find({}).fetch();
+	   	// generation = generation.sort(function(a,b){return a.generation_n < b.generation_n})[0];
+
+	   	//console.log(generation);
+
+	   	generation.crossover();
+	   	generation.mutation();
+	   	generation.estimation();
+	   	generation.selection();
+
+	    //console.log(generation.products_arr);
+
+	    generation.products_arr.forEach(function (product) {
+	    	if(target_util < product.product_util){
+	    		target_util = product.product_util;
+	    	}
+	    });
+
+	   	Generations.insert({
+	   		features_arr: generation.features_arr,
+	   		customers_arr: generation.customers_arr,
+	   		products_arr: generation.products_arr,
+	   		generation_n: time_count,
 	   	});
 
 
-	   	var products = [];
-
-	   	Products.find().fetch().forEach(function (product) {
-			products.push(product);
-		});
+	 //   	if(target_util == 162){
+		//     Meteor.clearInterval(interval);
+		// }
 
 
-		selection(products);
+
+
+
+
+	   	//////////////////////////////////////////////////////////////////////////////////////////////////
+	   	//////////////////////////////////////////////////////////////////////////////////////////////////
+	   	//////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+
+	 //   	var products = [];
+		// // var features = [];
+		// // var customers = [];
+
+		// Products.find().fetch().forEach(function (product) {
+		// 	products.push(product);
+		// });
+
+		// crossover(products);
+
+
+		// var products = [];
+
+	 //   	Products.find().fetch().forEach(function (product) {
+		// 	products.push(product);
+		// });
+
+	 //   	mutation(products);
+
+
+
+	 //   	Customers.find({}).fetch().forEach(function (customer) {
+	 //   		Customers.update(customer._id,{
+		//    		customer_id: customer.customer_id,
+		// 		customer_income: customer.customer_income,
+		// 		customer_product: customer.selectProduct(),
+		// 		needed: customer.needed,
+		//    	});
+	 //   	});
+
+	 // //   	Customers.find().fetch().forEach(function (customer) {
+		// // 	customers.push(customer);
+		// // });
+
+		// // Features.find().fetch().forEach(function (feature) {
+		// // 	features.push(feature);
+		// // });
+
+
+	 //   	Products.find({}).fetch().forEach(function (product) {
+	 //   		Products.update(product._id, {
+		// 		product_id: product.product_id,
+		// 		product_name: product.product_name,
+		// 		product_price: product.product_price,
+		// 		prop: product.prop,
+		// 		product_creator: product.product_creator,
+		//         product_status: product.product_status,
+		//         //product_share: product.getShare(),
+		//         product_util: product.getUtil(),
+		// 	});
+	 //   	});
+
+
+	 //   	var products = [];
+
+	 //   	Products.find().fetch().forEach(function (product) {
+		// 	products.push(product);
+		// });
+
+
+		// selection(products);
 
 
 	   	console.log("-----------------------------   END   --------------------------------");
-
-	}, 10000);
+	//}
+	}, 30000);
 
 
 
